acc9ea30d61147ee83859044e8ff215f7acfce1a
ritikamotwani <iit2015096@iiita.ac.in>
2017-07-29 23:04:51 +0530

    Foundation

---
+++ colors.js
@@ -0,0 +1,53 @@
+var col = generateRandomColors(6);
+var messageDisplay = document.getElementById("message");
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
+for (var i = 0; i < squares.length; i++)
+{
+	squares[i].style.background = col[i];
+	squares[i].addEventListener("click", function()
+	{
+		var clickedColor = this.style.backgroundColor;
+		if(clickedColor === pickedColor)
+		{
+			messageDisplay.textContent = "Correct";
+			changeColors(clickedColor);
+		}
+		else
+		{
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again";
+		}
+		
+	});
+}
+function changeColors(colo)
+{
+	for(var i = 0; i < squares.length; i++)
+	{
+		squares[i].style.background = colo;
+	}
+}
+function pickColor()
+{
+	var random = Math.floor(Math.random() * col.length);
+	return col[random];
+}
+function generateRandomColors(num)
+{
+	var arr = [];
+	for(var i = 0; i < num; i++)
+	{
+		arr.push(randomColor())
+	}
+	return arr;
+}
+function randomColor()
+{
+	var r = Math.floor(Math.random()*256);
+	var g = Math.floor(Math.random()*256);
+	var b = Math.floor(Math.random()*256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
